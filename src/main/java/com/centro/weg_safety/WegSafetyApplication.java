0f9ac1e742f3c348b28a8e0f2dad29e93d087f53
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-05-18 15:59:28 -0300

    feat: inicia versionamento ao projeto

---
+++ src/main/java/com/centro/weg_safety/WegSafetyApplication.java
@@ -0,0 +1,13 @@
+package com.centro.weg_safety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegSafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegSafetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centro/weg_safety/WegSafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centro.weg_safety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegSafetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
