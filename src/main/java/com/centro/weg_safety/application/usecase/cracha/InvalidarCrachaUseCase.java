d47d59e069eacfe1ff040d9a7f88418acdeab5e7
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:34:00 -0300

    feat: implement InvalidarCrachaUseCase

--- src/main/java/com/centro/weg_safety/application/usecase/cracha/InvalidarCrachaUseCase.java
+++ src/main/java/com/centro/weg_safety/application/usecase/cracha/InvalidarCrachaUseCase.java
@@ -1,4 +1,27 @@
 package com.centro.weg_safety.application.usecase.cracha;
 
+import com.centro.weg_safety.domain.exception.RecursoNaoEncontradoException;
+import com.centro.weg_safety.domain.model.enums.cracha.StatusCracha;
+import com.centro.weg_safety.infra.persistence.jpa.CrachaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
 public class InvalidarCrachaUseCase {
+
+    private final CrachaRepository crachaRepository;
+
+    public InvalidarCrachaUseCase(CrachaRepository crachaRepository) {
+        this.crachaRepository = crachaRepository;
+    }
+
+    @Transactional
+    public void execute(UUID id) {
+        var cracha = crachaRepository.findById(id)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Crachá não encontrado"));
+        cracha.setStatus(StatusCracha.INATIVO);
+        crachaRepository.save(cracha);
+    }
 }
