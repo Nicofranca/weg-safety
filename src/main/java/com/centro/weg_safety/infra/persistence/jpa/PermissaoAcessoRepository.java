6aa532c8bdb924e7037e4cfeba3ea08e86e2a20d
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:27:33 -0300

    feat: add custom queries to PermissaoAcessoRepository

--- src/main/java/com/centro/weg_safety/infra/persistence/jpa/PermissaoAcessoRepository.java
+++ src/main/java/com/centro/weg_safety/infra/persistence/jpa/PermissaoAcessoRepository.java
@@ -1,11 +1,17 @@
 package com.centro.weg_safety.infra.persistence.jpa;
 
 import com.centro.weg_safety.domain.model.PermissaoAcesso;
+import com.centro.weg_safety.domain.model.enums.permissaoAcesso.TipoPermissao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PermissaoAcessoRepository extends JpaRepository<PermissaoAcesso, UUID> {
+    List<PermissaoAcesso> findByAreaId(UUID areaId);
+    List<PermissaoAcesso> findByFuncionarioId(UUID funcionarioId);
+    Optional<PermissaoAcesso> findByAreaIdAndFuncionarioIdAndTipo(UUID areaId, UUID funcionarioId, TipoPermissao tipo);
 }
