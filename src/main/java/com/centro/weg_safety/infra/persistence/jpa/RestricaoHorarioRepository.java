4cb11b9c15b01f7c2d0468a2bf0404ad8da66186
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:28:36 -0300

    feat: add findByAreaId query to RestricaoHorarioRepository

--- src/main/java/com/centro/weg_safety/infra/persistence/jpa/RestricaoHorarioRepository.java
+++ src/main/java/com/centro/weg_safety/infra/persistence/jpa/RestricaoHorarioRepository.java
@@ -4,8 +4,10 @@ import com.centro.weg_safety.domain.model.RestricaoHorario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RestricaoHorarioRepository extends JpaRepository<RestricaoHorario, UUID> {
+    List<RestricaoHorario> findByAreaId(UUID areaId);
 }
