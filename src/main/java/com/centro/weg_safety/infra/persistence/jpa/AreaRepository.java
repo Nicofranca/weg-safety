5b415deaf1ba4115fb4d7e1e6daeed429d3dc462
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:27:22 -0300

    feat: add custom queries to AreaRepository

--- src/main/java/com/centro/weg_safety/infra/persistence/jpa/AreaRepository.java
+++ src/main/java/com/centro/weg_safety/infra/persistence/jpa/AreaRepository.java
@@ -1,11 +1,16 @@
 package com.centro.weg_safety.infra.persistence.jpa;
 
 import com.centro.weg_safety.domain.model.Area;
+import com.centro.weg_safety.domain.model.enums.area.StatusArea;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AreaRepository extends JpaRepository<Area, UUID> {
+    Optional<Area> findByCodigo(String codigo);
+    List<Area> findByStatus(StatusArea status);
 }
