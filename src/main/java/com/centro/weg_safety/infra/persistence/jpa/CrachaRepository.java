46a946c2eacdad0478685846ccc561df1b37c5d1
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:26:39 -0300

    feat: add custom queries to CrachaRepository

--- src/main/java/com/centro/weg_safety/infra/persistence/jpa/CrachaRepository.java
+++ src/main/java/com/centro/weg_safety/infra/persistence/jpa/CrachaRepository.java
@@ -1,11 +1,17 @@
 package com.centro.weg_safety.infra.persistence.jpa;
 
 import com.centro.weg_safety.domain.model.Cracha;
+import com.centro.weg_safety.domain.model.enums.cracha.StatusCracha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CrachaRepository extends JpaRepository<Cracha, UUID> {
+    List<Cracha> findByFuncionarioId(UUID funcionarioId);
+    Optional<Cracha> findByFuncionarioIdAndStatus(UUID funcionarioId, StatusCracha status);
+    Optional<Cracha> findByUidRfid(String uidRfid);
 }
