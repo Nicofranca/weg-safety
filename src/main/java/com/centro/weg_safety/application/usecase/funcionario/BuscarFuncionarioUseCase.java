592f701ad66ca11084357365ad955b276d87b49c
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:32:39 -0300

    feat: implement BuscarFuncionarioUseCase

--- src/main/java/com/centro/weg_safety/application/usecase/funcionario/BuscarFuncionarioUseCase.java
+++ src/main/java/com/centro/weg_safety/application/usecase/funcionario/BuscarFuncionarioUseCase.java
@@ -1,4 +1,27 @@
 package com.centro.weg_safety.application.usecase.funcionario;
 
+import com.centro.weg_safety.application.dto.response.funcionario.FuncionarioResponse;
+import com.centro.weg_safety.application.mapper.FuncionarioMapper;
+import com.centro.weg_safety.domain.exception.RecursoNaoEncontradoException;
+import com.centro.weg_safety.infra.persistence.jpa.FuncionarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class BuscarFuncionarioUseCase {
+
+    private final FuncionarioRepository funcionarioRepository;
+    private final FuncionarioMapper funcionarioMapper;
+
+    public BuscarFuncionarioUseCase(FuncionarioRepository funcionarioRepository, FuncionarioMapper funcionarioMapper) {
+        this.funcionarioRepository = funcionarioRepository;
+        this.funcionarioMapper = funcionarioMapper;
+    }
+
+    public FuncionarioResponse execute(UUID id) {
+        return funcionarioRepository.findById(id)
+                .map(funcionarioMapper::toResponse)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Funcionário não encontrado"));
+    }
 }
