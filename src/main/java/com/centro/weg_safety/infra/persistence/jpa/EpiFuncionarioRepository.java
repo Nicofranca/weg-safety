543137ce9980ce5cc2b48028be57d9c470dbcb3c
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:27:10 -0300

    feat: add custom queries to EpiFuncionarioRepository

--- src/main/java/com/centro/weg_safety/infra/persistence/jpa/EpiFuncionarioRepository.java
+++ src/main/java/com/centro/weg_safety/infra/persistence/jpa/EpiFuncionarioRepository.java
@@ -4,8 +4,13 @@ import com.centro.weg_safety.domain.model.EpiFuncionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EpiFuncionarioRepository extends JpaRepository<EpiFuncionario, UUID> {
+    List<EpiFuncionario> findByFuncionarioId(UUID funcionarioId);
+    List<EpiFuncionario> findByDataValidadeBefore(LocalDate data);
+    List<EpiFuncionario> findByDataValidadeBetween(LocalDate inicio, LocalDate fim);
 }
