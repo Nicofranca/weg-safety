b9bcaea0222f6a61dcd18cd6b7789a1da21976c3
T4yson <igor_savero@estudante.sesisenai.org.br>
2026-06-08 14:25:52 -0300

    feat: implement OpenApiConfig with Bearer JWT security scheme

--- src/main/java/com/centro/weg_safety/infra/conf/OpenApiConfig.java
+++ src/main/java/com/centro/weg_safety/infra/conf/OpenApiConfig.java
@@ -1,4 +1,30 @@
 package com.centro.weg_safety.infra.conf;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("WegSafety API")
+                        .description("Sistema de controle de acesso industrial com RFID")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
 }
